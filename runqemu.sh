03f22c5208fc95a25bb7b745895242eb5da7f300
joag6558 <joag6558@github.com>
2022-09-28 13:10:29 -0600

    a5 p2 v3

--- runqemu.sh
+++ runqemu.sh
@@ -2,6 +2,7 @@
 #Script to run QEMU for buildroot as the default configuration qemu_aarch64_virt_defconfig
 #Host forwarding: Host Port 10022 ->> QEMU Port 22 
 #Author: Siddhant Jajoo.
+#modification to port forwading
 
 
 qemu-system-aarch64 \
