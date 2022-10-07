1f99b36996cfab21308f7cfdedffa2dc1b75c0a9
joag6558 <joag6558@github.com>
2022-10-07 05:54:41 -0600

    a5 p2 aesdsocket fix timing

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 70070b8ff119bd15fd3a79b88682362a7c61f6a1
+AESD_ASSIGNMENTS_VERSION = d05dc9df2cf7dba3a05a2c03738f7ea18c7450c4
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
