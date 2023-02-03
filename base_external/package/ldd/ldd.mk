d1a92692b2342f13ec5d9098b155679bd9f56efd
joag6558 <joag6558@github.com>
2023-02-02 22:04:28 -0700

    a7 p2 buildroot ldd.mk

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = a32f942aee3c63df7e72e2afe76255a501104ed8
+LDD_VERSION = 938761db52c794fd3ccb9f8951dcf21e4779a3f7
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
